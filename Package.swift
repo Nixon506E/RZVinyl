9cc8359435d19b00d8d06cf9a99107e2b4c34f2e
Keith Lamprecht <1894492+Nixon506E@users.noreply.github.com>
2021-09-22 12:33:49 -0400

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,33 @@
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+import PackageDescription
+
+let package = Package(
+    name: "RZVinyl",
+    platforms: [
+        .iOS(.v8),
+    ],
+    products: [
+        .library(
+            name: "Core",
+            targets: ["Core"]),
+        .library(
+            name: "Extensions",
+            targets: ["Extensions"]),
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/Nixon506E/RZImport.git", 
+            from: "2.0.0"
+        )
+    ],
+    targets: [
+        .target(
+            name: "Core",
+            path: "Classes"),
+        .target(
+            name: "Extensions",
+            dependencies: ["Core","RZImport"],
+            path: "Extensions"),
+    ]
+)
